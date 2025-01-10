51404e3e9bf9ec12a4bfbd5c3292bbb08fe719e7
tungpv <tungpv@mirabo-global.com>
2025-01-10 14:55:05 +0700

    push to login page

--- src/components/ActiveBook/useActiveBook.js
+++ src/components/ActiveBook/useActiveBook.js
@@ -1,10 +1,17 @@
 import Services from "@/services";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const useActiveBook = () => {
   const [activeBooks, setActiveBooks] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
+    if (!localStorage.getItem("accessToken")) {
+      router.push("/dang-nhap");
+      return;
+    }
+
     Services.bookService
       .getActiveBooks()
       .then((books) => {
